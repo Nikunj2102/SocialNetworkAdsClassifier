04342558d67f147a5d1600e4daf45c896e71e6ba
Nikunj2102 <nikunjdhingra21@gmail.com>
2020-08-12 23:17:50 +0530

    files replaced

--- python files/DecisionTrees.py
+++ python files/DecisionTrees.py
@@ -3,14 +3,15 @@
 
 # IMPORT STATEMENTS
 
-# In[143]:
+# In[20]:
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
+from statistics import mean
+from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
 from sklearn import tree
 from sklearn.ensemble import BaggingClassifier
@@ -23,138 +24,91 @@ from sklearn.metrics import classification_report
 
 # IMPORT DATASET
 
-# In[144]:
+# In[21]:
 
 
 datasets = pd.read_csv('Social_Network_Ads.csv')
 datasets.describe()
 
 
-# In[145]:
-
-
-X_ref = datasets.iloc[:,[2,3]].head()
-
-
-# In[146]:
-
-
-Y_ref = datasets.iloc[:,4].head(10)
-
-
-# In[147]:
+# In[22]:
 
 
 X = datasets.iloc[:, [2,3]].values
 Y = datasets.iloc[:, 4].values
 
-
-# In[148]:
-
-
-#Splitting the dataset into testing and training datasets
-X_Train,X_Test,Y_Train,Y_Test = train_test_split(X,Y, test_size = 0.25, random_state = 0)
-
-
-# Feature scaling
-
-# In[149]:
-
-
-sc_X = StandardScaler()
-
-
-# In[150]:
-
-
-X_Train = sc_X.fit_transform(X_Train)
-X_Test = sc_X.transform(X_Test)
-
-
-# FITTING THE CLASSIFIER INTO THE TRAINING SET
-
-# In[151]:
-
-
-classifier = tree.DecisionTreeClassifier()
-classifierBagging = BaggingClassifier(n_estimators = 125 , random_state = 0)
-classifier.fit(X_Train,Y_Train)
-classifierBagging.fit(X_Train,Y_Train)
-
-
-# ---------------------------------------
-
-# PREDICTING THE TEST SET RESULTS
-
-# In[152]:
-
-
-Y_Pred = classifier.predict(X_Test)
-Y_Pred_Bagging = classifierBagging.predict(X_Test)
-
-
-# ----
-
-# MAKING CONFUSION MATRIX
-
-# In[153]:
-
-
-cm = confusion_matrix(Y_Test, Y_Pred)
-cm_bagging = confusion_matrix(Y_Test,Y_Pred_Bagging)
-print("W/O", cm,"\n")
-print("W:", cm_bagging)
-
-
-# In[154]:
-
-
-X_Set, Y_Set = X_Test, Y_Test
-
-
-# In[155]:
-
-
-X1, X2 = np.meshgrid(np.arange(start = X_Set[:, 0].min() - 1, stop = X_Set[:, 0].max() + 1, step = 0.01),
+kf = KFold(n_splits = 5)
+f1score = []
+f1scoreBagging = []
+for train_index , test_index in kf.split(X):
+    X_Train,X_Test = X[train_index],X[test_index]
+    Y_Train,Y_Test = Y[train_index],Y[test_index]
+    
+    #Feature Scaling
+    sc_X = StandardScaler()
+    X_Train = sc_X.fit_transform(X_Train)
+    X_Test = sc_X.transform(X_Test)
+    
+    #FITTING THE CLASSIFIER INTO THE TRAINING SET
+    
+    classifier = tree.DecisionTreeClassifier()
+    classifierBagging = BaggingClassifier(n_estimators = 125 , random_state = 0)
+    classifier.fit(X_Train,Y_Train)
+    classifierBagging.fit(X_Train,Y_Train)
+    
+    #PREDICTING THE TEST SET RESULTS
+    Y_Pred = classifier.predict(X_Test)
+    Y_Pred_Bagging = classifierBagging.predict(X_Test)
+    
+    #Making confusion matrix
+    cm = confusion_matrix(Y_Test, Y_Pred)
+    cm_bagging = confusion_matrix(Y_Test,Y_Pred_Bagging)
+    print("W/O", cm,"\n")
+    print("W:", cm_bagging)
+    
+    X_Set, Y_Set = X_Test, Y_Test
+    
+    #Plotting
+    X1, X2 = np.meshgrid(np.arange(start = X_Set[:, 0].min() - 1, stop = X_Set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_Set[:, 1].min() - 1, stop = X_Set[:, 1].max() + 1, step = 0.01))
 
-
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('pink', 'yellow')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(Y_Set)):
-    plt.scatter(X_Set[Y_Set == j, 0], X_Set[Y_Set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Decision Tree Classifier (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
-
-
-# In[156]:
-
-
-plt.contourf(X1, X2, classifierBagging.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('pink', 'yellow')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(Y_Set)):
-    plt.scatter(X_Set[Y_Set == j, 0], X_Set[Y_Set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Bagged Decision Tree (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
-
-
-# In[157]:
-
-
-print("With Bagging: ",classification_report(Y_Test,Y_Pred_Bagging),"\n")
-print("Without Bagging: ",classification_report(Y_Test,Y_Pred))
+    #Without bagging
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                 alpha = 0.75, cmap = ListedColormap(('pink', 'yellow')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(Y_Set)):
+        plt.scatter(X_Set[Y_Set == j, 0], X_Set[Y_Set == j, 1],
+                    c = ListedColormap(('red', 'green'))(i), label = j)
+    plt.title('Decision Tree Classifier (Test set)')
+    plt.xlabel('Age')
+    plt.ylabel('Estimated Salary')
+    plt.legend()
+    plt.show()
+    
+    #With Bagging
+    plt.contourf(X1, X2, classifierBagging.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                 alpha = 0.75, cmap = ListedColormap(('pink', 'yellow')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(Y_Set)):
+        plt.scatter(X_Set[Y_Set == j, 0], X_Set[Y_Set == j, 1],
+                    c = ListedColormap(('red', 'green'))(i), label = j)
+    plt.title('Bagged Decision Tree (Test set)')
+    plt.xlabel('Age')
+    plt.ylabel('Estimated Salary')
+    plt.legend()
+    plt.show()
+    
+    #Calculating the F1 Score
+    varBag = classification_report(Y_Test,Y_Pred_Bagging,output_dict=True)['weighted avg']['f1-score']
+    f1scoreBagging.append(varBag)
+    
+    var = classification_report(Y_Test,Y_Pred,output_dict=True)['weighted avg']['f1-score']
+    f1score.append(var)
+    
+print("Mean F1-Score(Bagging) :", mean(f1scoreBagging),"\n")
+print("Mean F1-Score(Without) :", mean(f1score))
 
 
 # In[ ]:
--- python files/Logistic.py
+++ python files/Logistic.py
@@ -3,14 +3,15 @@
 
 # IMPORT STATEMENTS
 
-# In[1]:
+# In[53]:
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from statistics import mean
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression as lr
 from sklearn.metrics import confusion_matrix
@@ -21,119 +22,67 @@ from sklearn.metrics import classification_report
 
 # IMPORT DATASET
 
-# In[2]:
+# In[54]:
 
 
 datasets = pd.read_csv('Social_Network_Ads.csv')
 datasets.describe()
 
 
-# In[3]:
-
-
-X_ref = datasets.iloc[:,[2,3]].head()
-
-
-# In[4]:
-
-
-Y_ref = datasets.iloc[:,4].head(10)
-
-
-# In[5]:
+# In[55]:
 
 
 X = datasets.iloc[:, [2,3]].values
 Y = datasets.iloc[:, 4].values
 
 
-# In[6]:
+# In[56]:
 
 
-#Splitting the dataset into testing and training datasets
-X_Train,X_Test,Y_Train,Y_Test = train_test_split(X,Y, test_size = 0.25, random_state = 0)
-
-
-# Feature scaling
-
-# In[7]:
-
-
-sc_X = StandardScaler()
-
-
-# In[8]:
-
-
-X_Train = sc_X.fit_transform(X_Train)
-X_Test = sc_X.transform(X_Test)
-
-
-# FITTING THE CLASSIFIER INTO THE TRAINING SET
-
-# In[9]:
-
-
-classifier = lr()
-classifier.fit(X_Train,Y_Train)
-
-
-# ---------------------------------------
-
-# PREDICTING THE TEST SET RESULTS
-
-# In[10]:
-
-
-Y_Pred = classifier.predict(X_Test)
-
-
-# ----
-
-# MAKING CONFUSION MATRIX
-
-# In[11]:
-
-
-cm = confusion_matrix(Y_Test, Y_Pred)
-print(cm)
-
-
-# In[12]:
-
-
-X_Set, Y_Set = X_Test, Y_Test
-
-
-# In[13]:
-
-
-X1, X2 = np.meshgrid(np.arange(start = X_Set[:, 0].min() - 1, stop = X_Set[:, 0].max() + 1, step = 0.01),
+kf = KFold(n_splits = 5)
+f1score = []
+for train_index , test_index in kf.split(X):
+    X_Train,X_Test = X[train_index],X[test_index]
+    Y_Train,Y_Test = Y[train_index],Y[test_index]
+    
+    #Feature Scaling
+    sc_X = StandardScaler()
+    X_Train = sc_X.fit_transform(X_Train)
+    X_Test = sc_X.transform(X_Test)
+    
+    #Fitting the classifier into the training set
+    classifier = lr()
+    classifier.fit(X_Train,Y_Train)
+    
+    #predicting the test set results
+    Y_Pred = classifier.predict(X_Test)
+    
+    #Making the confusion matrix
+    print("Confusion Matrix\n")
+    cm = confusion_matrix(Y_Test, Y_Pred)
+    print(cm)
+    
+    X_Set, Y_Set = X_Test, Y_Test
+    
+    #Plotting
+    X1, X2 = np.meshgrid(np.arange(start = X_Set[:, 0].min() - 1, stop = X_Set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_Set[:, 1].min() - 1, stop = X_Set[:, 1].max() + 1, step = 0.01))
 
 
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('pink', 'yellow')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(Y_Set)):
-    plt.scatter(X_Set[Y_Set == j, 0], X_Set[Y_Set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regression Classifier (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
-
-
-# In[14]:
-
-
-print(classification_report(Y_Test,Y_Pred))
-
-
-# In[ ]:
-
-
-
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                 alpha = 0.75, cmap = ListedColormap(('pink', 'yellow')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(Y_Set)):
+        plt.scatter(X_Set[Y_Set == j, 0], X_Set[Y_Set == j, 1],
+                    c = ListedColormap(('red', 'green'))(i), label = j)
+    plt.title('Logistic Regression Classifier (Test set)')
+    plt.xlabel('Age')
+    plt.ylabel('Estimated Salary')
+    plt.legend()
+    plt.show()
+    
+    var = classification_report(Y_Test,Y_Pred,output_dict=True)['weighted avg']['f1-score']
+    f1score.append(var)
+print("Avg F1-score for 5-folds: ",mean(f1score))
 
